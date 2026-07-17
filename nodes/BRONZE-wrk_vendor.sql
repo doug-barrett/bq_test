4810fc80a09de7e3de8931eabcac2f15d9aea029
Douglas Barrett <douglas.barrett@coalesce.io>
2026-07-17 07:50:19 -0600

    added 1 node, modified 1 node type, modified 1 macro

---
+++ nodes/BRONZE-wrk_vendor.sql
@@ -0,0 +1,6 @@
+@id("65c564fc-c5f5-445e-9643-1272b68b3a73")
+@nodeType("468")
+SELECT
+     "vendor_id" AS "vendor_id",
+     "vendor_name" AS "vendor_name"
+FROM {{ ref('BRONZE', 'vendor') }} `vendor`
