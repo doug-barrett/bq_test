662dd3cc676472742becc96ff5838466e06a0b97
Douglas Barrett <douglas.barrett@coalesce.io>
2026-09-25 11:45:09 -0600

    added 1 node, modified 1 node, modified 1 node type, added 1 package

---
+++ nodes/SILVER-wrk_location1.sql
@@ -0,0 +1,10 @@
+@id("d6b6e1c5-0921-47d3-b03f-5bf33a2e16e1")
+@nodeType("BaseSQL:::705")
+@materializationType("table")
+
+SELECT
+     `location_id` AS `location_id`,
+     `borough` AS `borough`,
+     `zone` AS `zone`,
+     `service_zone` AS `service_zone`
+FROM {{ ref('BRONZE', 'location') }} `location`
